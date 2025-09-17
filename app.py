1343e1fd7344caf7a44e12358c94152919ac8e9a
Rishabh .M <59rishabh@gmail.com>
2025-09-17 23:06:22 +0530

    Added ability to choose Continent, Year and Gas

--- app.py
+++ app.py
@@ -6,21 +6,25 @@ import matplotlib.pyplot as plt
 
 
 app=Flask(__name__)
-d={'CHN':0,'IND':0,'AUS':0}
+
 CT_API='https://api.climatetrace.org/v7/sources/'
 
 @app.route('/update', methods=["GET","POST"])
 def updatingplots():
     if request.method=="POST":
+        d={}
+        cont=request.form['Cont']
+        gaaas=request.form['Gases']
+        years=int(request.form['Year'])
         val=requests.get(CT_API, params={
-        "year": "2023",
-        "gas": "co2",
+        f"year": {years},
+        "gas": {gaaas},
         "sectors": "",
         "subsectors": "",
         "gadmId": "",
         "cityId": "",
         "countryGroup": "",
-        "continent": "",
+        "continent": {cont},
         "ownerIds": "",
         "limit": "50000",
         "offset": "0"
@@ -29,14 +33,20 @@ def updatingplots():
         for i in final_val:
             cntry=i['country']
             d[cntry]=d.get(cntry,0)+i['emissionsQuantity']
-            if len(d)>14:
+        finald = dict(sorted(d.items(), key=lambda item: item[1]))
+        finald = dict(reversed(finald.items()))
+
+        actually_the_final_dict={}
+        for i in finald:
+            actually_the_final_dict[i]=finald[i]
+            if len(actually_the_final_dict)>9:
                 break
-    
-        keys=list(d.keys())
-        vals=list(d.values())
+        
+        keys=list(actually_the_final_dict.keys())
+        vals=list(actually_the_final_dict.values())
 
         plt.bar(keys,vals)
-        plt.title("Carbon emissions")
+        plt.title(f" Emissions ({gaaas})")
         plt.savefig("static/plot.png")
         plt.close()
 
